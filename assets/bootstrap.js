79c13118949c37bdb27d70d3ea99e996d1cb0825
nicky <nickyrasa@gmail.com>
2024-01-04 05:59:19 +0100

    first commit

--- assets/bootstrap.js
+++ assets/bootstrap.js
@@ -1,5 +1,11 @@
-import { startStimulusApp } from '@symfony/stimulus-bundle';
-
-const app = startStimulusApp();
+import { startStimulusApp } from "@symfony/stimulus-bundle";
+export const app = startStimulusApp(
+	require.context(
+		"@symfony/stimulus-bridge/lazy-controller-loader!./controllers",
+		true,
+		/\.(j|t)sx?$/
+	)
+);
+// const app = startStimulusApp();
 // register any custom, 3rd party controllers here
 // app.register('some_controller_name', SomeImportedController);
